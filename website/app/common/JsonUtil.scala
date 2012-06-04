32acd8ba3947085e904984c4de1fae12a32d4fa3
Christophe Bram <cbramdit@gmail.com>
2012-06-05 00:12:00 +0200

    Changed JsonUtil to the same as the Showroom

--- website/app/common/JsonUtil.scala
+++ website/app/common/JsonUtil.scala
@@ -1,34 +1,43 @@
 package common
 
-import java.io.InputStream
-import org.codehaus.jackson.JsonNode
 import org.codehaus.jackson.map.ObjectMapper
-import org.codehaus.jackson.node.ObjectNode
 import com.codahale.jerkson.ScalaModule
+import play.api.libs.json.{Json, JsValue}
+import play.api.Logger
+import play.api.libs.ws.Response
+
 
 object JsonUtil {
-  def parse[T](inputStream: InputStream, classOfT: Class[T]) = {
-    createObjectMapper().readValue(inputStream, classOfT)
+  def parse[T](json: String, classOfT: Class[T]) = {
+    try {
+      createObjectMapper().readValue(json, classOfT)
+    } catch {
+      case e: Throwable => {
+        Logger.error("Unable to parse JSON into " + classOfT.getName + ": " + json, e); throw e
+      }
+    }
   }
 
-  def parse[T](json: String, classOfT: Class[T]) = {
-    createObjectMapper().readValue(json, classOfT)
+  def parseResponse[T](response: Response, classOfT: Class[T]) = {
+    if (response.header("Content-Type").isDefined && response.header("Content-Type").get.startsWith("application/json")) {
+      try {
+        createObjectMapper().readValue(response.getAHCResponse.getResponseBodyAsStream, classOfT)
+      } catch {
+        case e: Throwable => {
+          Logger.error("Unable to parse JSON into " + classOfT.getName, e); throw e
+        }
+      }
+    } else {
+      throw new Exception("Expecting JSON, but got " + response.body)
+    }
+  }
+
+  def parseJsValue[T](json: JsValue, classOfT: Class[T]) = {
+    parse(Json.stringify(json), classOfT)
   }
 
   def serialize(obj: AnyRef): String = {
     createObjectMapper().writeValueAsString(obj)
-  }
-
-  def parse(inputStream: InputStream): JsonNode = {
-    createObjectMapper().readTree(inputStream)
-  }
-
-  def parse(json: String): JsonNode = {
-    createObjectMapper().readTree(json)
-  }
-
-  def copyObjectNode(obj: ObjectNode): ObjectNode = {
-    createObjectMapper().readTree(obj.traverse()).asInstanceOf[ObjectNode]
   }
 
   private def createObjectMapper() = {
